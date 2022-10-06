3c23b5d6a5a05ab99398efe2a498ab050eea94ab
Chris <firstof9@gmail.com>
2022-10-06 13:50:20 -0700

    adjust tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -87,25 +87,7 @@ async def test_add_code_zwave_js(hass, client, lock_kwikset_910, integration):
     args = client.async_send_command.call_args[0][0]
     assert args["command"] == "node.set_value"
     assert args["nodeId"] == 14
-    assert args["valueId"] == {
-        "ccVersion": 1,
-        "commandClassName": "User Code",
-        "commandClass": 99,
-        "endpoint": 0,
-        "property": "userCode",
-        "propertyName": "userCode",
-        "propertyKey": 1,
-        "propertyKeyName": "1",
-        "metadata": {
-            "type": "string",
-            "readable": True,
-            "writeable": True,
-            "minLength": 4,
-            "maxLength": 10,
-            "label": "User Code (1)",
-        },
-        "value": "123456",
-    }
+    assert args["valueId"] == {'commandClass': 99, 'endpoint': 0, 'property': 'userCode', 'propertyKey': 1}
     assert args["value"] == "1234"
 
 
@@ -141,25 +123,9 @@ async def test_clear_code_zwave_js(hass, client, lock_kwikset_910, integration):
     assert args["command"] == "node.set_value"
     assert args["nodeId"] == 14
     assert args["valueId"] == {
-        "ccVersion": 1,
-        "commandClassName": "User Code",
-        "commandClass": 99,
-        "endpoint": 0,
-        "property": "userIdStatus",
-        "propertyName": "userIdStatus",
-        "propertyKey": 1,
-        "propertyKeyName": "1",
-        "metadata": {
-            "type": "number",
-            "readable": True,
-            "writeable": True,
-            "label": "User ID status (1)",
-            "states": {
-                "0": "Available",
-                "1": "Enabled",
-                "2": "Disabled",
-            },
-        },
-        "value": 1,
+        'commandClass': 99,
+        'endpoint': 0,
+        'property': 'userIdStatus',
+        'propertyKey': 1,
     }
     assert args["value"] == 0
